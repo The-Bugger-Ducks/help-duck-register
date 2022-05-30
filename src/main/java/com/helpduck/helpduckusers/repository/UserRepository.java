3e6ae8fafa0054bc0dcdb89010201b4f6a9a654a
Thiago Henrique Ferreira <henriquethiago298@gmail.com>
2022-05-30 15:25:45 -0300

    filter user list per username and role, per username and per role (#19)

--- src/main/java/com/helpduck/helpduckusers/repository/UserRepository.java
+++ src/main/java/com/helpduck/helpduckusers/repository/UserRepository.java
@@ -3,9 +3,22 @@ package com.helpduck.helpduckusers.repository;
 import java.util.Optional;
 
 import com.helpduck.helpduckusers.entity.User;
+import com.helpduck.helpduckusers.enums.RoleEnum;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface UserRepository extends MongoRepository<User, String> {
   Optional<User> findByEmail(String email);
+
+  // { }
+  @Query("{ $or: [ {'firstName': { $regex:?0, '$options' : 'i' }}, {'lastName': { $regex:?0, '$options' : 'i' }} ]}")
+  Page<User> searchUsername(Pageable page, String username);
+
+  @Query("{ $or: [ {'firstName': { $regex:?0, '$options' : 'i' }}, {'lastName': { $regex:?0, '$options' : 'i' }} ],  $and: [{'role': ?1 }] }")
+  Page<User> searchUsernameAndFilterPerRole(Pageable page, String username, RoleEnum role);
+
+  Page<User> findByRole(Pageable page, RoleEnum role);
 }
